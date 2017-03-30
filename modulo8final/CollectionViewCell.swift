c5f6ca4b5d0833385622359952a96c639c1c99ae
Raymundo Peralta <movile@peta.mx>
2017-03-29 21:50:40 -0600

    projecto final modulo 8

---
+++ modulo8final/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  modulo8final
+//
+//  Created by Raymundo Peralta on 3/29/17.
+//  Copyright © 2017 Raymundo Peralta. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ modulo8final/MuestraCuadros.swift
@@ -0,0 +1,35 @@
+//
+//  MuestraCuadros.swift
+//  modulo8final
+//
+//  Created by Raymundo Peralta on 3/29/17.
+//  Copyright © 2017 Raymundo Peralta. All rights reserved.
+//
+
+import UIKit
+
+class MuestraCuadros: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
